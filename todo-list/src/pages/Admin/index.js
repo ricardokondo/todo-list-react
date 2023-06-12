6c22af56fc6499b7dff7dd4dabf3997826857d6c
Ricardo Kondo <rimkondo@gmail.com>
2023-06-12 13:56:05 -0400

    Registrando no banco de dados

--- todo-list/src/pages/Admin/index.js
+++ todo-list/src/pages/Admin/index.js
@@ -1,9 +1,78 @@
-import './admin.css'
+import { useState, useEffect } from "react";
+import "./admin.css";
 
-export default function Admin(){
-  return(
-    <div>
-      <h1>Pagina ADMIN!</h1>
+import { auth, db } from "../../firebaseConnection";
+import { signOut } from "firebase/auth";
+
+import { addDoc, collection } from "firebase/firestore";
+
+export default function Admin() {
+  const [tarefaInput, setTarefaInput] = useState("");
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem("@detailUser");
+      setUser(JSON.parse(userDetail));
+    }
+
+    loadTarefas();
+  }, []);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    if (tarefaInput === "") {
+      alert("Digite sua tarefa...");
+      return;
+    }
+
+    await addDoc(collection(db, "tarefas"), {
+      tarefa: tarefaInput,
+      created: new Date(),
+      userUid: user?.uid,
+    })
+      .then(() => {
+        console.log("TAREFA REGISTRADA");
+        setTarefaInput("");
+      })
+      .catch((error) => {
+        console.log("ERRO AO REGISTRAR " + error);
+      });
+  }
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
+
+  return (
+    <div className="admin-container">
+      <h1>Minhas tarefas</h1>
+
+      <form className="form" onSubmit={handleRegister}>
+        <textarea
+          placeholder="Digite sua tarefa..."
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        />
+
+        <button className="btn-register" type="submit">
+          Registrar tarefa
+        </button>
+      </form>
+
+      <article className="list">
+        <p>Estudar javascript e reactjs hoje a noite</p>
+
+        <div>
+          <button>Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>
+        Sair
+      </button>
     </div>
-  )
+  );
 }
