f64ac1e8c48f3fe7b274aad163f6c39d2e2551ea
Ricardo Kondo <rimkondo@gmail.com>
2023-06-12 13:16:27 -0400

    Criando página home, register e admin

--- todo-list/src/routes/index.js
+++ todo-list/src/routes/index.js
@@ -1,15 +1,17 @@
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import Home from '../pages/Home';
-import Register from '../pages/Register';
+import Home from "../pages/Home";
+import Register from "../pages/Register";
+import Admin from "../pages/Admin";
 
-function RoutesApp(){
-  return(
+function RoutesApp() {
+  return (
     <Routes>
-      <Route path='/' element={ <Home/> } />
-      <Route path='/register' element={ <Register/> } />
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
-  )
+  );
 }
 
 export default RoutesApp;
